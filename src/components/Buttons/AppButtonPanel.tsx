6a9c4d52c37ff2cd1a7b4401f765c1210cca96ce
Lucas Santos <lucas.silva1203@outlook.com>
2023-01-31 17:24:57 -0300

    Add callback and state params to the component

--- src/components/Buttons/AppButtonPanel.tsx
+++ src/components/Buttons/AppButtonPanel.tsx
@@ -1,10 +1,18 @@
 import styles from "./AppButtonPanel.module.css";
 
-export const AppButtonPanel = () => {
+export const AppButtonPanel = ({
+    toggleModal,
+    isActive,
+}: {
+    toggleModal: Function;
+    isActive: boolean;
+}) => {
     return (
         <div className={styles["button-panel"]}>
             <button className={styles["add-btn"]}>Add</button>
-            <button className={styles["close-btn"]}>Close</button>
+            <button className={styles["close-btn"]} onClick={()=> {
+                toggleModal(!isActive)
+            }}>Close</button>
         </div>
     );
 };
