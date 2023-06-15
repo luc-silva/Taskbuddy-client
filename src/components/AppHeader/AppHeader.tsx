435458ae577d0435237dc51794b95690ebb8705d
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-15 17:26:33 -0300

    refactor: change logo title

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -3,7 +3,7 @@ import styles from "./AppHeader.module.css";
 export const AppHeader = () => {
     return (
         <header className={styles["app-header"]}>
-            <h1>Lucas' Todo-List</h1>
+            <h1>Taskbuddy</h1>
             <a href="https://github.com/luc-silva" target={"_blank"}>
                 Github
             </a>
