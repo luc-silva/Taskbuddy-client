ebc77f8d3d535e7e9cd2c2b6ccabe8a450bb5de0
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-15 17:26:13 -0300

    feat: add entities interfaces

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,39 @@
-declare module '*.module.css' {
+interface IUserSession extends IUser {
+    isLogged: boolean;
+}
+
+interface IUser {
+    name: string;
+    email: string;
+    id: number;
+}
+
+interface IProjectTask {
+    id: number;
+    title: string;
+    priority: string;
+    completed: false;
+}
+
+interface IProject {
+    id: number;
+    title: string;
+    description: string;
+    deadline: string;
+    priority: string;
+    user: IUser;
+    projectTasks: IProjectTask[];
+}
+
+interface ITodo {
+    text: string;
+    concluded: boolean;
+    deadline: string;
+    priority: string;
+    user: IUser;
+}
+
+declare module "*.module.css" {
     const classes: { [key: string]: string };
     export default classes;
-  }
+}
