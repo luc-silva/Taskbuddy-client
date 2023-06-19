a93996d67bfd7387e8ef936c61e1df2c5b2b514d
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-19 17:31:36 -0300

    feat: create TodoService class

---
+++ src/services/TodoService.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { Service } from "./Service";
+
+class TodoService extends Service {
+    public async get(id: number): Promise<ITodo> {
+        return await axios
+            .get(`${this.base_url}/todo/${id}`)
+            .then(({ data }) => {
+                return data;
+            });
+    }
+
+    public async delete(id: number): Promise<IMessageResponse> {
+        return await axios
+            .delete(`${this.base_url}/todo/${id}`)
+            .then(({ data }) => {
+                return data;
+            });
+    }
+
+    public async create(id: number, data: ITodo): Promise<ITodo> {
+        return await axios
+            .post(`${this.base_url}/todo/${id}`, data)
+            .then(({ data }) => {
+                return data;
+            });
+    }
+
+    public async update(id: number, data: ITodo): Promise<ITodo> {
+        return await axios
+            .put(`${this.base_url}/todo/${id}`, data)
+            .then(({ data }) => {
+                return data;
+            });
+    }
+}
+
+export default new TodoService();
