5966def1be3ba22ee3667f31a145a24db05348f7
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-19 16:32:40 -0300

    feat: add delete method

--- src/services/ProjectService.ts
+++ src/services/ProjectService.ts
@@ -2,10 +2,20 @@ import axios from "axios";
 import { Service } from "./Service";
 
 class ProjectService extends Service {
+    public constructor() {
+        super();
+    }
+
     public async get(id: number): Promise<IProject> {
         return await axios.get(`${this.base_url}/project/${id}`).then(({data}) => {
             return data;
         });
+    }
+
+    public async delete(id:number): Promise<IMessageResponse> {
+        return await axios.delete(`${this.base_url}/project/${id}`).then(({data}) => {
+            return data;
+        })
     }
 
 }
