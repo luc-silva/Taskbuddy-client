b9c04b34d5d3d5e6f2d82c6d89c218a05c6c1c97
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-19 15:58:26 -0300

    feat: create service base class and ProjectTaskService

---
+++ src/services/ProjectTaskService.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosPromise } from "axios";
+import { Service } from "./Service";
+
+class ProjectTaskService extends Service {
+    public constructor() {
+        super();
+    }
+    public async get(id: number): Promise<IProjectTask> {
+        return axios
+            .get(`${this.base_url}/project-task/${id}`)
+            .then(({ data }) => {
+                return data;
+            });
+    }
+
+    public async update(id: number, data: IProjectTask) {
+        return axios
+            .post(`${this.base_url}/project-task/${id}`, data)
+            .then(({ data }) => {
+                return data;
+            });
+    }
+}
+
+export default new ProjectTaskService();
---
+++ src/services/Service.ts
@@ -0,0 +1,5 @@
+export abstract class Service {
+    protected base_url = "http://localhost:8080/api/";
+
+    protected createHeader() {}
+}
